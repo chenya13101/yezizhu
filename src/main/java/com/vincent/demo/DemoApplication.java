53b744ef4027c46591a378d7a8c79779d593f291
wensen <846384270@qq.com>
2018-03-28 17:44:30 +0800

    springBoot自动配置的demo

---
+++ src/main/java/com/vincent/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.vincent.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello Spring boot";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		//访问 http://localhost:8080/
+	}
+}
