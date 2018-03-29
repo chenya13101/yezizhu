8fc52a05a9ff4a050f5de59dae522ebbc9640b24
wensen <846384270@qq.com>
2018-03-29 13:54:59 +0800

    ConfigurationProperties 默认读取的是application.properties文件内的属性

--- src/main/java/com/vincent/demo/component/settings/BookSettings.java
+++ src/main/java/com/vincent/demo/component/settings/BookSettings.java
@@ -3,6 +3,14 @@ package com.vincent.demo.component.settings;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * ConfigurationProperties 默认读取的是application.properties文件内的属性
+ * 
+ * @ClassName: BookSettings
+ * @author wensen
+ * @date 2018年3月29日 下午1:54:14
+ *
+ */
 @Component
 @ConfigurationProperties(prefix = "book")
 // 如果是配置在application.properties里不需要额外加属性locations
