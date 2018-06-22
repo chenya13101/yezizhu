624b28b341197e7f7ab1be6721c476e6e4c60cc8
wensen <846384270@qq.com>
2018-06-22 13:42:57 +0800

    提交

---
+++ ERP调整userID后修改券码数据.sql
@@ -0,0 +1,11 @@
+update t_coupon_code SET receive_user_id = '614105',
+receive_user_name = '13710665124'
+WHERE receive_vip_id = '1193173';
+
+update t_coupon_code SET receive_user_id = '613825',
+receive_user_name = '18277113245'
+WHERE receive_vip_id = '1196394';
+
+update t_coupon_code SET receive_user_id = '649657',
+receive_user_name = '18577080050'
+WHERE receive_vip_id = '1212936';
