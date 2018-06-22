1c1c47df176ffea873ce9ef3c24489fe76cb169e
wensen <846384270@qq.com>
2018-06-22 14:47:29 +0800

    增加一个人

--- ERP调整userID后修改券码数据.sql
+++ ERP调整userID后修改券码数据.sql
@@ -9,3 +9,8 @@ WHERE receive_vip_id = '1196394';
 update t_coupon_code SET receive_user_id = '649657',
 receive_user_name = '18577080050'
 WHERE receive_vip_id = '1212936';
+
+
+update t_coupon_code SET receive_user_id = '554834',
+receive_user_name = '18648801382'
+WHERE receive_vip_id = '1192917';
