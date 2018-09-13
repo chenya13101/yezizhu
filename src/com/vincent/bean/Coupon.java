3478d356913bab56a390b80da7b3bcd517925c83
wensen <846384270@qq.com>
2018-09-13 11:24:38 +0800

    分割  html传入的参数与系统内实际使用的类型

--- src/com/vincent/bean/Coupon.java
+++ src/com/vincent/bean/Coupon.java
@@ -1,6 +1,6 @@
 package com.vincent.bean;
 
-import com.vincent.common.CouponTypeEnum;
+import com.vincent.bean.inwardType.UseLimitInward;
 
 public class Coupon {
 
@@ -10,19 +10,34 @@ public class Coupon {
 
 	private PromotionRange promotionRange;
 
-	private CouponTypeEnum typeEnum; // TODO 优惠券类型，新版本会出现许多不一致的属性，难度增加
+	private int type; // CouponTypeEnum index
 
 	// TODO 这里可以把优惠券类型与具体的使用限制关联起来
 
 	private UseLimit useLimit; // 使用限制 包括了 优惠金额，最低金额要求
 
-	public Coupon(String code, String name, PromotionRange promotionRange, CouponTypeEnum typeEnum, UseLimit useLimit) {
+	// TODO 需要考虑前端html如何将数据传入，并且以何种形式展示给前端.同时不能产生混淆
+	public Coupon(String code, String name, PromotionRange promotionRange, int type, UseLimit useLimit) {
 		super();
 		this.code = code;
 		this.name = name;
 		this.promotionRange = promotionRange;
-		this.typeEnum = typeEnum;
-		this.useLimit = useLimit;
+		this.type = type;
+		this.useLimit = useLimit;// TODO 这里传入的可以是一个复杂对象，包含三个的通用属性，但是在这里被转化为具体的Limit
+	}
+
+	public Coupon(String code, String name, PromotionRange promotionRange, int type, UseLimitInward useLimitInward) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.promotionRange = promotionRange;
+		this.type = type;
+		this.useLimit = changeLimitInwardToLimit(type, useLimitInward);
+	}
+
+	private UseLimit changeLimitInwardToLimit(int type, UseLimitInward useLimitInward) {
+		// TODO 这里传入的可以是一个复杂对象，包含三个的通用属性，但是在这里被转化为具体的Limit
+		return null;
 	}
 
 	public UseLimit getUseLimit() {
@@ -41,12 +56,12 @@ public class Coupon {
 		this.code = code;
 	}
 
-	public CouponTypeEnum getTypeEnum() {
-		return typeEnum;
+	public int getType() {
+		return type;
 	}
 
-	public void setTypeEnum(CouponTypeEnum typeEnum) {
-		this.typeEnum = typeEnum;
+	public void setType(int type) {
+		this.type = type;
 	}
 
 	public String getName() {
---
+++ src/com/vincent/bean/inwardType/UseLimitInward.java
@@ -0,0 +1,12 @@
+package com.vincent.bean.inwardType;
+
+/**
+ * 封装使用限制参数，可能是外部想要传入的 红包 折扣券 代金券的任何一种的限制条件. 作为一个中间层,分割[外部的传入]与[系统内实际使用的对象]
+ * 
+ * @author WenSen
+ * @date 2018年9月13日 上午11:19:45
+ *
+ */
+public class UseLimitInward {
+
+}
